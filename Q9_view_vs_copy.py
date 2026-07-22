e4d189d6188a3fb73776bf55a8af455eb4e070bc
Hoda Nageh <hodanageh12@gmail.com>
2026-07-22 15:14:31 +0300

    Add files via upload

---
+++ Q9_view_vs_copy.py
@@ -0,0 +1,33 @@
+# pixel_ level image 
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read image
+img = cv2.imread("/home/hoda/Pictures/Screenshots/Screenshot from 2026-07-12 16-31-35.png")
+
+part = img[100:200, 100:200]
+
+part[:] = (255, 0, 0)
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("Bug")
+plt.axis("off")
+plt.show()
+
+img2 = cv2.imread("image.jpg")
+
+part2 = img2[100:200, 100:200].copy()
+
+part2[:] = (255, 0, 0)
+
+plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+plt.title("Fix")
+plt.axis("off")
+plt.show()
+
+img3 = cv2.imread("image.jpg")
+
+print("Bug :", np.array_equal(img, img3))
+print("Fix :", np.array_equal(img2, img3))
